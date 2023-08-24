d6e89af756016d7087f3d9d7c11b7f67fd4e74cb
한해용 <yong7317>
2023-08-24 18:03:17 +0900

    Fix: 피드 API 구현
    피드에 사용자 공개 여부 조건 추가

--- src/main/java/com/dnd/gooding/domain/feed/service/FeedServiceImpl.java
+++ src/main/java/com/dnd/gooding/domain/feed/service/FeedServiceImpl.java
@@ -3,6 +3,7 @@ package com.dnd.gooding.domain.feed.service;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import com.dnd.gooding.domain.record.model.RecordOpenStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
@@ -27,6 +28,7 @@ public class FeedServiceImpl implements FeedService {
 	public Page<FeedResponse> findByRecordByInterestCodeAndIsNotUserId(Long userId, List<InterestType> interestCodes, Pageable pageable) {
 		Page<Record> records = feedRepository.findByRecordByInterestCodeAndIsNotUserId(userId, interestCodes, pageable);
 		return new PageImpl<>(records.stream()
+				.filter(record -> RecordOpenStatus.PUBLIC.name().equals(record.getRecordOpen().name()))
 				.map(FeedResponse::new)
 				.collect(Collectors.toList()),
 				pageable, records.getTotalElements());
