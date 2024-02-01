8f9e3d5c76e5c822dc0aec3050a2a0b8f76a9922
haeyonghahn <yong80211@gmail.com>
2024-02-01 23:45:12 +0900

    Chore: 빌드 수정

--- src/test/java/com/dnd/gooding/integration/GoodingApplicationTest.java
+++ src/test/java/com/dnd/gooding/integration/GoodingApplicationTest.java
@@ -1,7 +1,9 @@
 package com.dnd.gooding.integration;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 class GoodingApplicationTest extends IntegrationTest {
 
   @Test
--- src/test/java/com/dnd/gooding/integration/user/command/CreateMemberServiceIntegrationTest.java
+++ src/test/java/com/dnd/gooding/integration/user/command/CreateMemberServiceIntegrationTest.java
@@ -9,7 +9,9 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 class CreateMemberServiceIntegrationTest extends IntegrationTest {
 
   @Autowired private CreateMemberService createMemberService;
