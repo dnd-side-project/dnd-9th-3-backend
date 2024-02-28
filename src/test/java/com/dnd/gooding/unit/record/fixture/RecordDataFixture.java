5e04a072618436338c115b829dd249c21c1884d6
haeyonghahn <yong80211@gmail.com>
2024-02-28 21:57:14 +0900

    Fix: 기록하기 테스트 오류 수정
    Related to: #52

--- src/test/java/com/dnd/gooding/unit/record/fixture/RecordDataFixture.java
+++ src/test/java/com/dnd/gooding/unit/record/fixture/RecordDataFixture.java
@@ -1,18 +1,10 @@
 package com.dnd.gooding.unit.record.fixture;
 
-import com.dnd.gooding.record.command.domain.Image;
 import com.dnd.gooding.record.query.dto.RecordData;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 public class RecordDataFixture {
 
     public static RecordData getRecord() {
-        List<Image> images = new ArrayList<>();
-        images.add(new Image("http://qwe123123.jpg", LocalDateTime.now()));
-        images.add(new Image("http://qwe12315153.jpg", LocalDateTime.now()));
-
         return RecordData.builder()
                 .recordNumber("123qwaf124515")
                 .placeTitle("바다")
@@ -22,7 +14,6 @@ public class RecordDataFixture {
                 .recorderName("haeyong")
                 .title("기록1")
                 .description("기록1 설명")
-                .images(images)
                 .build();
     }
 }
