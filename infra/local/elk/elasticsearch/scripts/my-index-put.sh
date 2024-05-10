0309f68f3b577171d579e74d2e71a17d5a5769f6
haeyonghahn <yong80211@gmail.com>
2024-05-10 12:32:36 +0900

    Chore: elk 설정 추가

--- infra/local/elk/elasticsearch/scripts/my-index-put.sh
+++
@@ -1,6 +0,0 @@
-:set fileformat=unix
-
-until "curl -XPUT localhost:9200/record_index_v1?pretty -H 'Content-Type: application/json' -d /my_index.json"; do
-  echo "# waiting for elasticsearch - $(date)"
-  sleep 3
-done
