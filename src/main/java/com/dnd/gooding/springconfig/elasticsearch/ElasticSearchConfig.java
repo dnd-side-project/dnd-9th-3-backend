0130222501bcfc3bf482381d305eccdaa8a92a87
한해용 <yong7317>
2024-05-10 18:05:00 +0900

    Chore: springboot elasticsearch 라이브러리 추가

---
+++ src/main/java/com/dnd/gooding/springconfig/elasticsearch/ElasticSearchConfig.java
@@ -0,0 +1,9 @@
+package com.dnd.gooding.springconfig.elasticsearch;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+@Configuration
+@EnableElasticsearchRepositories
+public class ElasticSearchConfig {
+}
