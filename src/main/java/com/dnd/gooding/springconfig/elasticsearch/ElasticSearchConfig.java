815c50725f7576defaff52a5cd911104abf70558
haeyonghahn <yong80211@gmail.com>
2024-05-21 22:31:24 +0900

    Remove: Elastic 설정 삭제

--- src/main/java/com/dnd/gooding/springconfig/elasticsearch/ElasticSearchConfig.java
+++
@@ -1,9 +0,0 @@
-package com.dnd.gooding.springconfig.elasticsearch;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
-
-@Configuration
-@EnableElasticsearchRepositories
-public class ElasticSearchConfig {
-}
